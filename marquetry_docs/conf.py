2f939864e68a802a902c5dc26a77942712707813
SHIMA0111 <shima@little-tabby.com>
2023-10-22 11:21:06 +0900

    [Backport] add version tag

--- marquetry_docs/conf.py
+++ marquetry_docs/conf.py
@@ -54,6 +54,10 @@ html_context = {
     "support_languages": {
         "en": "English",
         "ja": "日本語",
+    },
+    "versions": {
+        "0.1.0": "v0.1.0",
+        "0.2.0": "v0.2.0"
     }
 }
 
